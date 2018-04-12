33921f11966bca2fee1b2df34087b320197b49b8
dck26 <daniel.cuadra@gmail.com>
2018-04-11 21:11:23 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/pav.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <?php
+    include "modulos/menu-principal.php";
+  ?>
+  <div class="parallax"></div>
+  <div class="container">
+
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis eleifend arcu, sed ullamcorper urna. Aliquam pellentesque lorem eros, nec pellentesque nisl varius eu. Nulla urna libero, ullamcorper id ullamcorper non, luctus id nisi. Ut pretium egestas enim, in ultrices neque placerat at. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis elementum egestas odio, at egestas velit tempor in. Nunc sodales eros odio, sed blandit nulla suscipit sed.
+
+Etiam lacinia a velit sed dignissim. Aenean vitae eros et mauris placerat iaculis. Praesent feugiat egestas nisl non tempor. Donec lorem lorem, elementum id ligula eu, eleifend aliquet lectus. Praesent tristique mi massa, eu ornare est hendrerit sit amet. Nullam in enim sed orci euismod tempus ac a arcu. Pellentesque lorem ante, eleifend sit amet ligula varius, condimentum porta libero. Praesent aliquam eros erat, ac placerat augue convallis quis. Ut id viverra ligula. In gravida iaculis libero, eu pharetra tellus congue eu. Vivamus luctus lacus dolor, sed consequat nisl faucibus convallis.
+
+Aenean bibendum tortor non enim malesuada blandit. Sed eleifend placerat nisi sed elementum. Nullam maximus nisi sed consequat convallis. In eu dolor odio. Curabitur id est commodo, aliquet massa sed, elementum nisi. Nunc ut posuere ante, eget ultricies tellus. Etiam malesuada pharetra dui, quis eleifend leo mattis ut. Vestibulum semper hendrerit nulla ac tincidunt.
+
+Etiam et massa sit amet arcu viverra faucibus quis quis erat. Nulla facilisi. Vivamus pellentesque in tortor in tincidunt. Nunc sit amet neque bibendum, varius libero sit amet, fringilla lorem. Vivamus lacus turpis, efficitur vitae dui euismod, malesuada interdum eros. Morbi ac ipsum sagittis, dignissim arcu sit amet, ultricies risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In cursus scelerisque mi, non malesuada ligula lacinia et. Praesent a erat nec tellus interdum pretium. Morbi fringilla efficitur nibh, quis iaculis odio tincidunt ac. Donec leo ligula, consectetur non ullamcorper eu, convallis ac libero. Duis eu ullamcorper turpis. Sed tincidunt nunc eu massa efficitur congue. Mauris id auctor lectus, faucibus auctor est. Cras viverra eget libero faucibus porta.
+
+Curabitur ante nisl, efficitur et porta ut, sollicitudin id sapien. Nam vitae sem at ipsum fringilla venenatis eu a nibh. Vestibulum aliquet, libero id porta mollis, dolor turpis tempus justo, varius dapibus nisl lectus vitae ex. Nullam viverra, quam eu sodales tincidunt, odio elit bibendum nisl, efficitur hendrerit felis massa in turpis. Maecenas nec augue augue. Sed et congue urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel sagittis justo, quis condimentum leo. Sed sed justo urna. Nullam vel convallis neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi vel auctor lorem. Fusce vulputate sagittis nunc vel auctor. Mauris maximus vulputate aliquet.
+</p>
+    <!-- Container element -->
+
+</div>
+
+</body>
+</html>
---
+++ modulos/menu-principal.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-inverse navbar-pav navbar-fixed-top">
+  <div class="container-fluid containter-fluid-pav">
+    <ul class="nav navbar-nav" style="background-color: black;">
+      <li><a href="#"><i class="fas fa-bars fa-1x"></i></a></li>
+    </ul>
+    <div class="navbar-header">
+      <a class="navbar-brand navbar-brand-pav" href="#"><img src="img/logo.jpg"></a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">NOSOTROS</a></li>
+      <li><a href="#">SERVICIOS</a></li>
+      <li><a href="#">PRODUCTOS</a></li>
+      <li><a href="#">SOCIOS COMERCIALES</a></li>
+      <li><a href="#">CONTACTO</a></li>
+      <li><a href="#" class="elemento-negro">COMPRAR EN LÍNEA</a></li>
+    </ul>
+  </div>
+</nav>
